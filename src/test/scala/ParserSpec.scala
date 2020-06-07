fe1845701c4316dfef3d8d47b3dd636b952476ce
Arnout Engelen <arnout@bzzt.net>
2020-06-07 19:56:23 +0200

    Remove duplicate imports

--- src/test/scala/ParserSpec.scala
+++ src/test/scala/ParserSpec.scala
@@ -4,9 +4,9 @@ import org.scalatest._
 
 class ParserSpec extends WordSpec with Matchers {
   "The parser" should {
-    "parse 3 levels of indentation into the correct tree" in {
-      import Parser._
+    import Parser._
 
+    "parse 3 levels of indentation into the correct tree" in {
       Parser.interpretIndentation("test.txt", """
         |foo
         |  bar
@@ -16,8 +16,6 @@ class ParserSpec extends WordSpec with Matchers {
     }
 
     "parse returning to the previous level of indentation" in {
-      import Parser._
-
       Parser.interpretIndentation("test.txt", """
         |foo
         |  bar
@@ -32,8 +30,6 @@ class ParserSpec extends WordSpec with Matchers {
     }
 
     "warn about invalid indentation" in {
-      import Parser._
-
       try {
         Parser.interpretIndentation("test.txt", """
           |foo
@@ -48,8 +44,6 @@ class ParserSpec extends WordSpec with Matchers {
     }
 
     "tokenize 'is an' as a separate token" in {
-      import Parser._
-
       val tree = Parser.tokenize(Tree("r is an int", Seq.empty, Location.unknown))
       tree.top should be ("r")
       tree.children.size should be(2)
@@ -58,8 +52,6 @@ class ParserSpec extends WordSpec with Matchers {
     }
 
     "tokenize parens as separate subtrees" in {
-      import Parser._
-
       val trees = Parser.interpretIndentation("test.txt", """
           |foo quux
           |  bar (pom piedom)
@@ -78,47 +70,36 @@ class ParserSpec extends WordSpec with Matchers {
     }
 
     "parse a named variable of primitive type" in {
-      import Parser._
-
       val tree = Parser.tokenize(Tree("r is an int", Seq.empty, Location.unknown))
       Parser.parseAliasOpt(tree, Context.empty) should be(Some(Alias(Integer, "r", auto = false)))
       Parser.parseType(tree, Context.empty) should be(Alias(Integer, "r", auto = false))
     }
 
     "parse a named variable of composite type" in {
-      import Parser._
-
       val compositeType = Record("color", Seq.empty)
       val tree = Parser.tokenize(Tree("r is a color", Seq.empty, Location.unknown))
       Parser.parseAliasOpt(tree, Context(compositeType)) should be(Some(Alias(compositeType, "r", auto = false)))
     }
 
     "parse an assignment of a literal to a primitive variable" in {
-      import Parser._
-
       val trees = Parser.interpretIndentation("test.vs", "int = 42").map(Parser.tokenize)
       val (_, segments) = Parser.parseSegments(trees, Context.empty)
       segments(0) should be(Assignment("int", Literal(42, Integer), Integer))
     }
 
     "parse an assignment of a literal to a variable with an explicit type" in {
-      import Parser._
-
       val trees = Parser.interpretIndentation("test.vs", "answer is an int = 42").map(Parser.tokenize)
       val (_, segments) = Parser.parseSegments(trees, Context.empty)
       segments(0) should be(Assignment("answer", Literal(42, Integer), Integer))
     }
 
     "parse an assignment of a complex type to a variable with an explicit type" in {
-      import Parser._
-
       val trees = Parser.interpretIndentation("test.vs", "answer is a color = color 255 0 0 255").map(Parser.tokenize)
       val (_, segments) = Parser.parseSegments(trees, Context.empty)
       segments(0) should be(Assignment("answer", MethodCall("color", List(Literal(255, Integer), Literal(0, Integer), Literal(0, Integer), Literal(255, Integer)), null), null))
     }
 
     "parse a parameter with parens" in {
-      import Parser._
       val trees = Parser.interpretIndentation("test.vs", """
         |width is an int
         |height is an int
@@ -132,8 +113,6 @@ class ParserSpec extends WordSpec with Matchers {
     }
 
     "parse an expression referring to a variable" in {
-      import Parser._
-
       val trees = Parser.interpretIndentation("test.vs", "answer is an int = 42\nprintln answer").map(Parser.tokenize)
       val (_, segments) = Parser.parseSegments(trees, Context.empty)
       segments(1) should be(MethodCall("println", Seq(Reference("answer", Integer)), null))
